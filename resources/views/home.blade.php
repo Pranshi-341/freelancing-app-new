c9534545c0f7e17f0da98c111f47f988e0e57e78
TasteVibes <tastevibescodes@gmail.com>
2022-06-19 14:01:39 +0530

    first changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+<style>
+   .head-part {
+      /* background-image: url(https://asset.edusson.com/bundles/asterfreelance/_layout/images/EdussonCom/intro-v4/intro-bg@2x.webp); */
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+   }
+</style>
+@section('content')
+         <!-- <div class="row">
+            <div class="col-md-12 head-part">
+               <h3>Get a unique essay!</h3>
+               <p>Hire our professional writer to save time</p>
+               <form action="" method="get" style="display:flex;flex-direction:row; padding:6px;border:1px solid grey;background-color:white;width: 56%;">
+                  <input type="text" style="width: 68%;height:40px;border: none" placeholder="search your staf"/>
+                     <button style="border:1px solid blue;background:blue;color:white;">
+                        <span class="d-none-mobile">Hire Writer</span>
+                     </button>
+               </form>
+               <ul class="intro-v4__tip-list">
+                        <li class="intro-v4__tip">Easy Process</li>
+                        <li class="intro-v4__tip">24/7 on Demand</li>
+                        <li class="intro-v4__tip">Timesaver</li>
+               </ul>
+            </div>
+         </div> -->
+@endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-image: url(https://asset.edusson.com/bundles/asterfreelance/_layout/images/EdussonCom/intro-v4/intro-bg@2x.webp);">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,3 +29,22 @@
     </div>
   </div>
 </nav>
+<div class="row">
+            <div class="col-md-12 head-part">
+               <h3>Get a unique essay!</h3>
+               <p>Hire our professional writer to save time</p>
+               <form action="" method="get" style="display:flex;flex-direction:row; padding:6px;border:1px solid grey;background-color:white;width: 56%;">
+                  <input type="text" style="width: 68%;height:40px;border: none" placeholder="search your staf"/>
+                  <!-- <div class="search-form-v2__buttons-wrap"> -->
+                     <button style="border:1px solid blue;background:blue;color:white;">
+                        <span class="d-none-mobile">Hire Writer</span>
+                     </button>
+                  <!-- </div> -->
+               </form>
+               <ul class="intro-v4__tip-list">
+                        <li class="intro-v4__tip">Easy Process</li>
+                        <li class="intro-v4__tip">24/7 on Demand</li>
+                        <li class="intro-v4__tip">Timesaver</li>
+               </ul>
+            </div>
+         </div>
