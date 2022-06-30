d54079e3a160a14da6820e1ac36b46b5e5c5b598
Sarv1997 <57605837+Sarv1997@users.noreply.github.com>
2022-06-30 19:14:18 +0530

    Contact us form

--- resources/views/frontend/contactpage.blade.php
+++ resources/views/frontend/contactpage.blade.php
@@ -12,7 +12,8 @@
    }
 </style>
 @section('content')
-   <div style="background-size:cover; background-image: {{$backgroundImage}}">
+<div class="row">
+   <div class="col-md-12" style="background-size:cover; background-image: {{$backgroundImage}}">
       @include('layouts.navbar')
       <div class="col-md-12 mt-5 head-part text-white">
          <h2>{{$title}}</h2>
@@ -20,6 +21,37 @@
          <p style="color: #ffc107;">{{$bottomline}}</p>
       </div>
    </div>
-   <div class="bg-light">
+</div>
+<!-- create contact us form -->
+<div class="row">
+   <div class="col-sm-12 p-5">
+      <div class="card">
+         <div class="card-body">
+            <h4 class="card-title text-center">Contact Us</h4>
+            <form class="form-material m-t-40" action="" method="post">
+               @csrf
+               <div class="form-group">
+                  <label>Name</label>
+                  <input type="text" class="form-control form-control-line" name="name" required>
+               </div>
+               <div class="form-group">
+                  <label>Email</label>
+                  <input type="email" class="form-control form-control-line" name="email" required>
+               </div>
+               <div class="form-group">
+                  <label>Phone</label>
+                  <input type="text" class="form-control form-control-line" name="phone" required>
+               </div>
+               <div class="form-group">
+                  <label>Message</label>
+                  <textarea class="form-control" rows="5" name="message" required></textarea>
+               </div>
+               <button type="submit" class="btn btn-success my-2">Submit</button>
+            </form>
+         </div>
+      </div>
    </div>
+</div>
+
+
 @endsection
