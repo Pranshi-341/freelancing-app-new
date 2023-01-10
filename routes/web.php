1b68c4b93d74b7c45b64cc36c10be0afbaa797b6
Pranshi-341 <pransikodegurus@gmail.com>
2023-01-10 16:04:37 +0530

    homes page,teams page content,issues

--- routes/web.php
+++ routes/web.php
@@ -110,6 +110,7 @@ Route::get('/professional-writers',[frontendController::class,'ProfessionalWrite
 Route::get('/competent-tutors',[frontendController::class,'CompetentTutors']);
 Route::get('/licensed-researchers',[frontendController::class,'LicensedResearchers']);
 
+
 // add route
 Route::get('/contactUs',[frontendController::class,'ContactUs']);
 Route::get('/aboutUs',[frontendController::class,'aboutUs']);
