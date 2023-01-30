4429ac3c91fe4b79e86ed5461ea765254ad9c3f7
Pranshi-341 <pransikodegurus@gmail.com>
2023-01-30 11:25:26 +0530

    chat...

--- resources/views/frontend/posts.blade.php
+++ resources/views/frontend/posts.blade.php
@@ -84,6 +84,7 @@
                                         
                                     @else
                                         <button disabled class="btn btn-success btn-sm">Order accepted</button>
+                                        
                                     @endif
                                     @endif
                                     {{-- add button view order --}}
@@ -98,7 +99,8 @@
                                     @if($post['bidPost'][0]['status'] == 0)
                                         <span class="badge bg-warning badge-warning">Waiting for Accepted</span>
                                     @elseif($post['bidPost'][0]['status'] == 1)
-                                        <span class="badge bg-success badge-success">accepted</span>
+                                        <!-- <span class="badge bg-success badge-success">accepted</span> -->
+                                        <button class="btn btn-success btn-sm">Chat</button>
                                     @elseif($post['bidPost'][0]['status'] == 2)
                                         <span class="badge badge-danger">rejected</span>
                                     @elseif($post['bidPost'][0]['status'] == 3)
